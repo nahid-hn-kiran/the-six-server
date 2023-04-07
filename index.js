3c1daf5fa3b581ade1b2800227cde48016e76571
nahid7g <nahidhasankiran2@gmail.com>
2023-04-07 13:50:30 -0400

    package.json updated

--- index.js
+++ index.js
@@ -3,22 +3,21 @@ const dotenv = require('dotenv')
 const express = require('express')
 const { default: mongoose } = require('mongoose')
 const { notFound, errorHandler } = require('./middlewares/errorHandler')
+const connectDB = require('./utills/connectDb')
 
 dotenv.config({
   path: 'config/config.env',
 })
 
 // Database connection
-mongoose.connect(process.env.LOCAL_DB).then(() => {
-  console.log('Database connection Succesful')
-})
+connectDB()
 
 // Middlewares
 // app.use(notFound)
 // app.use(errorHandler)
 app.use('/uploads/images', express.static('uploads/images'))
 
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 5000
 app.listen(port, () => {
   console.log(`Express is running from port: ${port}`)
 })
